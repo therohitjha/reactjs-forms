f071241c53e3afc0370f71c850e9823eadc7d6c6
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2019-05-02 12:05:46 +0530

    Updates

---
+++ precache-manifest.2ed91e9032cfd952795af4ab78b029ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af4bb4baf1a2d801cad218d542d4de0",
+    "url": "/reactjs-forms/index.html"
+  },
+  {
+    "revision": "1cd1ef29421b13baf49c",
+    "url": "/reactjs-forms/static/css/main.0aa59c97.chunk.css"
+  },
+  {
+    "revision": "5c7badd242f9ece752a2",
+    "url": "/reactjs-forms/static/js/2.adfc45b8.chunk.js"
+  },
+  {
+    "revision": "1cd1ef29421b13baf49c",
+    "url": "/reactjs-forms/static/js/main.23a992db.chunk.js"
+  },
+  {
+    "revision": "c2e1f3505491753c4be2",
+    "url": "/reactjs-forms/static/js/runtime~main.3f7f4eac.js"
+  }
+]);
